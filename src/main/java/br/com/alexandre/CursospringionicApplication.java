bfd1cfce4457fc51d71b98d41d2ebe3de9edae5c
Alexandre <jajrjunior95@gmail.com>
2020-04-05 17:32:04 -0300

    c1 - Projeto Iniciado

---
+++ src/main/java/br/com/alexandre/CursospringionicApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexandre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringionicApplication.class, args);
+	}
+
+}
