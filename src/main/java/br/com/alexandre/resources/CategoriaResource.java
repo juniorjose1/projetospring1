a629fe6bbb1f5849a18ee9b76ca913b97b034f6d
Alexandre <jajrjunior95@gmail.com>
2020-04-06 20:53:45 -0300

    c3 - Criando e Testando Classe de domínio - Categoria

--- src/main/java/br/com/alexandre/resources/CategoriaResource.java
+++ src/main/java/br/com/alexandre/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.alexandre.resources;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alexandre.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST funcionando !";
+	public ArrayList<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		ArrayList<Categoria> listaCategorias = new ArrayList<>();
+		
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
 	}
 
 }
