200a38f5537eb9ea522361ca7a4f10385754449a
Alexandre <jajrjunior95@gmail.com>
2020-05-24 21:32:18 -0300

    Adicionando Serializable em ItemPedidoPK

--- src/main/java/br/com/alexandre/domain/ItemPedidoPK.java
+++ src/main/java/br/com/alexandre/domain/ItemPedidoPK.java
@@ -1,12 +1,16 @@
 package br.com.alexandre.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Embeddable
-public class ItemPedidoPK {
-	
+public class ItemPedidoPK implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	@ManyToOne
 	@JoinColumn(name = "pedido_id")
 	private Pedido pedido;
